0f5e28f1a0ec3335b25d26207ccba25a5d7351df
Jaskaran Singh <sjaskaran10@yandex.com>
2020-03-03 16:38:37 +0530

    0.1 assed attempt

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const app = require("path");
+const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
@@ -50,7 +50,6 @@ app.use((req, res, next) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
