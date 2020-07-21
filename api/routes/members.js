4ae462930679238e6fbad7ea90577611a71f4010
Jaskaran Singh <yayjkay@gmail.com>
2020-07-21 13:43:32 +0530

    not accepting invalid input

--- api/routes/members.js
+++ api/routes/members.js
@@ -18,22 +18,32 @@ router.get("/allIds", (req, res, next) => {
 
 //create a member
 router.post("/", (req, res, next) => {
-  const member = new Members({
-    _id: mongoose.Types.ObjectId(),
-    team: req.body.team,
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    position: req.body.position,
-  });
+  if (
+    req.body.team &&
+    req.body.name &&
+    req.body.email &&
+    req.body.phone &&
+    req.body.position
+  ) {
+    const member = new Members({
+      _id: mongoose.Types.ObjectId(),
+      team: req.body.team,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      position: req.body.position,
+    });
 
-  member
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({ result, ok: 1 });
-    })
-    .catch((error) => res.status(500).json(error));
+    member
+      .save()
+      .then((result) => {
+        console.log(result);
+        res.status(200).json({ result, ok: 1 });
+      })
+      .catch((error) => res.status(500).json(error));
+  } else {
+    res.status(422).json({ message: "invalid input" });
+  }
 });
 
 module.exports = router;
